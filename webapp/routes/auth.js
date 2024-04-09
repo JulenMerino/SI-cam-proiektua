626bceb3abfef97b5d983c49530ab2204bbede70
aKijn2 <mrkinder20041010@gmail.com>
2024-04-09 12:57:25 +0200

    Google api soluzionatuta

--- webapp/routes/auth.js
+++ webapp/routes/auth.js
@@ -1,22 +1,21 @@
 var express = require("express");
 var passport = require("passport");
-var GoogleStrategy = require("passport-google-oidc").Strategy;
-var FacebookStrategy = require("passport-facebook").Strategy;
-
+var GoogleStrategy = require("passport-google-oidc");
+// var FacebookStrategy = require("passport-facebook").Strategy;
 var db = require("../db");
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env["GOOGLE_CLIENT_ID"],
+      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
       callbackURL: "/oauth2/redirect/google",
       scope: ["profile"],
     },
-    function (accessToken, refreshToken, profile, cb) {
+    function verify(issuer, profile, cb) {
       db.get(
         "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
-        ["https://accounts.google.com", profile.id],
+        [issuer, profile.id],
         function (err, row) {
           if (err) {
             return cb(err);
@@ -33,7 +32,7 @@ passport.use(
                 var id = this.lastID;
                 db.run(
                   "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
-                  [id, "https://accounts.google.com", profile.id],
+                  [id, issuer, profile.id],
                   function (err) {
                     if (err) {
                       return cb(err);
@@ -68,82 +67,78 @@ passport.use(
   )
 );
 
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: "/oauth2/redirect/facebook",
-      state: true,
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      db.get(
-        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
-        ["https://www.facebook.com", profile.id],
-        function (err, row) {
-          if (err) {
-            return cb(err);
-          }
-          if (!row) {
-            db.run(
-              "INSERT INTO users (name) VALUES (?)",
-              [profile.displayName],
-              function (err) {
-                if (err) {
-                  return cb(err);
-                }
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FACEBOOK_CLIENT_ID,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+//       callbackURL: "/oauth2/redirect/facebook",
+//       state: true,
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       db.get(
+//         "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+//         ["https://www.facebook.com", profile.id],
+//         function (err, row) {
+//           if (err) {
+//             return cb(err);
+//           }
+//           if (!row) {
+//             db.run(
+//               "INSERT INTO users (name) VALUES (?)",
+//               [profile.displayName],
+//               function (err) {
+//                 if (err) {
+//                   return cb(err);
+//                 }
 
-                var id = this.lastID;
-                db.run(
-                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
-                  [id, "https://www.facebook.com", profile.id],
-                  function (err) {
-                    if (err) {
-                      return cb(err);
-                    }
-                    var user = {
-                      id: id,
-                      name: profile.displayName,
-                    };
-                    return cb(null, user);
-                  }
-                );
-              }
-            );
-          } else {
-            db.get(
-              "SELECT * FROM users WHERE id = ?",
-              [row.user_id],
-              function (err, row) {
-                if (err) {
-                  return cb(err);
-                }
-                if (!row) {
-                  return cb(null, false);
-                }
-                return cb(null, row);
-              }
-            );
-          }
-        }
-      );
-    }
-  )
-);
+//                 var id = this.lastID;
+//                 db.run(
+//                   "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+//                   [id, "https://www.facebook.com", profile.id],
+//                   function (err) {
+//                     if (err) {
+//                       return cb(err);
+//                     }
+//                     var user = {
+//                       id: id,
+//                       name: profile.displayName,
+//                     };
+//                     return cb(null, user);
+//                   }
+//                 );
+//               }
+//             );
+//           } else {
+//             db.get(
+//               "SELECT * FROM users WHERE id = ?",
+//               [row.user_id],
+//               function (err, row) {
+//                 if (err) {
+//                   return cb(err);
+//                 }
+//                 if (!row) {
+//                   return cb(null, false);
+//                 }
+//                 return cb(null, row);
+//               }
+//             );
+//           }
+//         }
+//       );
+//     }
+//   )
+// );
 
 passport.serializeUser(function (user, cb) {
-  cb(null, user.id);
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
 });
 
-passport.deserializeUser(function (id, cb) {
-  db.get("SELECT * FROM users WHERE id = ?", [id], function (err, row) {
-    if (err) {
-      return cb(err);
-    }
-    if (!row) {
-      return cb(null, false);
-    }
-    return cb(null, row);
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
   });
 });
 
@@ -152,7 +147,6 @@ var router = express.Router();
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
-
 router.get("/login/federated/google", passport.authenticate("google"));
 router.get(
   "/oauth2/redirect/google",
@@ -162,18 +156,15 @@ router.get(
   })
 );
 
-router.get("/login/federated/facebook", passport.authenticate("facebook"));
-router.get(
-  "/oauth2/redirect/facebook",
-  passport.authenticate("facebook", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
-
 router.post("/logout", function (req, res, next) {
-  req.logout();
-  res.redirect("/");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
+
+// OLD
